fb242a7e2255949aaf2c59c8dff1d6d9cb404e0d
Mayra Cuenca <mayra.cuencac@gmail.com>
2019-06-17 21:11:46 -0500

    Added doctorsEdit, patientsEdit, prescriptionsList. Edited dictorsLogin, doctorsMenu

--- models/example.js
+++
@@ -1,9 +0,0 @@
-// models/example.js
-
-module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
-};
